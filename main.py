368710aebf5d09a32d9f75e3194b431753c85104
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-11-12 15:16:53 -0800

    feat: Add get cards route

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+my_cards = [{"subject": "Spanish", "question": "comer", "answer": "to eat", "active": True, "id": 1},
+{"subject": "Spanish", "question": "haber", "answer": "to speak", "active": True, "id": 2}]
 
 class Card(BaseModel):
     subject: str
@@ -15,7 +17,11 @@ class Card(BaseModel):
 async def root():
     return {"message": "Welcome to Leitner Box"}
 
+@app.get('/cards')
+def get_cards():
+    return {"data": my_cards}
+
 @app.post("/cards")
 def create_card(card: Card):
     print(card.dict())
-    return{"data":"card"}
+    return{"data": card}

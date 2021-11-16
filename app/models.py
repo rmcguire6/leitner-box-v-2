7332ea109a4210aafe787fba2495352cefa0a939
Regina McGuire <rmcguire@as.net>
2021-11-16 11:34:02 -0800

    refactor: Update card model with created_at column

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 class Card(Base):
@@ -8,4 +10,5 @@ class Card(Base):
   subject = Column(String(64), nullable=False)
   question = Column(String(120), nullable=False)
   answer = Column(String(120), nullable=False)
-  is_active = Column(Boolean, default=True)
+  is_active = Column(Boolean, server_default='TRUE', nullable=False)
+  created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))

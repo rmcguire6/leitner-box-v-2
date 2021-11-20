441a27ec1741e62cb6fb02da6d365b073b2a170f
Regina McGuire <rmcguire@as.net>
2021-11-19 16:57:38 -0800

    feat: Add Pydantic response model CardOut  to all routes that return cards

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -17,25 +18,25 @@ async def root():
     return {"message": "Welcome to Leitner Box"}
 
 
-@app.get('/cards')
+@app.get('/cards', response_model=List[schemas.CardOut])
 def get_cards(db: Session= Depends(get_db)):
     cards = db.query(models.Card).all()
-    return {"data": cards}
+    return cards
 
-@app.post("/cards", status_code=status.HTTP_201_CREATED)
-def create_card(card: schemas.Card, db: Session = Depends(get_db)):
+@app.post("/cards", status_code=status.HTTP_201_CREATED, response_model= schemas.CardOut)
+def create_card(card: schemas.CardBase, db: Session = Depends(get_db)):
     new_card = models.Card(**card.dict())
     db.add(new_card)
     db.commit()
     db.refresh(new_card)
-    return{"data": new_card}
+    return new_card
 
-@app.get('/cards/{id}')
+@app.get('/cards/{id}', response_model=schemas.CardOut)
 def get_card(id:int, db: Session = Depends(get_db)):
     card = db.query(models.Card).filter(models.Card.card_id == id).first()
     if not card:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"card with id {id} was not found")
-    return {"card": card}
+    return card
 
 @app.delete('/cards/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_card(id:int, db: Session = Depends(get_db)):
@@ -46,12 +47,12 @@ def delete_card(id:int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put('/cards/{id}')
-def update_card(id:int, card: schemas.Card,  db: Session = Depends(get_db)):
+@app.put('/cards/{id}', response_model=schemas.CardOut)
+def update_card(id:int, card: schemas.CardBase,  db: Session = Depends(get_db)):
     card_query = db.query(models.Card).filter(models.Card.card_id == id)
     found_card = card_query.first()
     if found_card == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"card with id {id} does not exist")
     card_query.update(card.dict(), synchronize_session=False)
     db.commit()
-    return {'message': card_query.first()}
+    return card_query.first()
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel
+from datetime import datetime
 
-class Card(BaseModel):
+class CardBase(BaseModel):
     subject: str
     question: str
     answer: str
     is_active: bool = True
+
+
+class CardOut(CardBase):
+  created_at: datetime
+  card_id: int
+
+  class Config:
+    orm_mode = True

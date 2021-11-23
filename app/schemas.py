1a15f1497f4c1909aa720a0e35211791dfae6a3d
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-11-23 15:04:43 -0800

    feat: Add verify_access_token function to oauth2

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 class CardBase(BaseModel):
     subject: str
@@ -34,3 +35,10 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
   email: EmailStr
   password: str
+
+class Token(BaseModel):
+  access_token: str
+  token_type: str
+
+class TokenData(BaseModel):
+  user_id: Optional[str] = None

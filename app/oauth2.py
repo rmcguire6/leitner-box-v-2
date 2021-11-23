d3b353ca576b1a92f316a6c816b2a48ab6c39d33
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-11-23 15:12:06 -0800

    bug: Fix errors in oauth2

--- app/oauth2.py
+++ app/oauth2.py
@@ -13,7 +13,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -26,11 +26,12 @@ def verify_access_token(token: str, credentials_exception):
 
         if id is None:
             raise credentials_exception
-            token_data = schemas.TokenData(id=id)
+        token_data = schemas.TokenData(id=id)
     except JWTError:
         raise credentials_exception
+    
     return token_data
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW Authenticate": "Bearer"})
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW Authenticate": "Bearer"})
     return verify_access_token(token, credentials_exception)

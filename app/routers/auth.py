4cee1dc64fa9ccb96801b126caa7654dee29be5b
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-11-23 14:32:35 -0800

    refactor: Add Oauth Password Request From

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from ..import database, schemas, models, utils, oauth2
@@ -6,8 +7,8 @@ from ..import database, schemas, models, utils, oauth2
 router = APIRouter(tags=["Authentication"])
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
     if not utils.verify(user_credentials.password, user.password):

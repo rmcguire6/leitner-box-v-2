4ba46d2e875774304a90ba963475689c726f4d14
Regina McGuire <rmcguire@as.net>
2021-12-29 11:34:07 -0800

    test: Create initial test

---
+++ tests/test_users.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient 
+from app.main import app
+
+client = TestClient(app)
+
+def test_root():
+  res = client.get("/")
+  print(res)

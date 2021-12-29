e04d2152673831adc00ca234f55597b0959d2de9
Regina McGuire <rmcguire@as.net>
2021-12-29 15:56:14 -0800

    test: Add test_create_user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient 
 from app.main import app
+from app import schemas
 
 client = TestClient(app)
 
 def test_root():
   res = client.get("/")
-  print(res)
+  print(res.json().get("message"))
+  assert res.json().get("message") == 'Welcome to Leitner Box'
+
+def test_create_user():
+  res = client.post("/users/", json={
+    "email": "abby@example.com",
+    "username": "Abby",
+    "cards_per_day": 3,
+    "password": "123pass"
+    })
+  print(res.json())
+  assert res.json().get("email") == "abby@example.com"
+  assert res.status_code == 201

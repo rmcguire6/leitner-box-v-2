a176d0df406acb966c6b6c199ac9720102e92f04
Regina McGuire <rmcguire@as.net>
2021-12-30 19:00:20 -0800

    test: Add more tests to incorrect login

--- tests/test_users.py
+++ tests/test_users.py
@@ -32,10 +32,31 @@ def test_login_user(client, test_user):
   assert login_res.token_type == 'bearer'
   assert res.status_code == 200
 
-def test_incorrect_login(test_user, client):
+@pytest.mark.parametrize("email, password, status_code", [
+  ('wrongemail@example.com', '123pass', 403),
+  ('abby@example.com', 'wrongpassword', 403),
+  ('wrongemail@example.com', 'wrongpassword', 403),
+  (None, '123pass', 422),
+  ('abby@example.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
   res = client.post("/login/", data={
-    "username":test_user['email'],
-    "password":"wrong password"
+    "username": email,
+    "password": password
   })
-  assert res.status_code == 403
-  assert res.json().get('detail') == 'Invalid Credentials'
+  assert res.status_code == status_code
+
+
+def test_get_user_by_id(test_user, client):
+  res = client.get("/users/1/")
+  found_user = schemas.UserOut(**res.json())
+  assert res.status_code == 200
+  assert found_user.user_id == test_user['user_id']
+  assert found_user.email == test_user['email']
+  assert found_user.username == test_user['username']
+  assert found_user.cards_per_day == test_user['cards_per_day']
+  assert found_user.current_day_number == test_user['current_day_number']
+
+def test_incorrect_user_id(test_user, client):
+  res = client.get("/users/-5")
+  assert res.status_code == 404

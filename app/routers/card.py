3eb2b96b8f7f82825be9da22eb63dda02d5c4e22
R McGuire <rmcguire@as.net>
2022-06-29 20:20:06 -0700

    bug: Correct error message for PUT route

--- app/routers/card.py
+++ app/routers/card.py
@@ -49,17 +49,18 @@ def delete_card(card_id:int, db: Session = Depends(get_db), current_user: int = 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put('/cards/{card_id}', response_model=schemas.CardOut)
-def update_card(card_id:int, card: schemas.CardBase,  db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def update_card(card_id:int, card: schemas.CardBase, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     card_query = db.query(models.Card).filter(models.Card.card_id == card_id)
     found_card = card_query.first()
     if found_card is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Card with id {id} does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Card with id {card_id} does not exist")
     if found_card.creator_id != current_user.user_id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Not authorized to perform requested action")
     card_query.update(card.dict(), synchronize_session=False)
     db.commit()
     return card_query.first()
+
 front_end_cards = [{
     "card_id": 1,
     "answer": 'to live',

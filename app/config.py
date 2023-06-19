92e128284e2e94cb9a14a5c1084597dd09350f35
RMcGuire <28935544+rmcguire6@users.noreply.github.com>
2023-06-19 13:15:39 -0700

    bug: Correct database url config error

--- app/config.py
+++ app/config.py
@@ -4,12 +4,13 @@ class Settings(BaseSettings):
   database_username: str
   database_password: str 
   database_hostname: str 
-  database_port: str 
+  database_port: int 
   database_name: str
   secret_key: str
   algorithm: str 
   access_token_expire_minutes: int
   front_end_url: str
+  database_url: str
 
   class Config:
     env_file = '.env'
